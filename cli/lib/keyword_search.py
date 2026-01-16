d51b99c81e6125b152b43c711d1662aa6fa373a3
Karim OCB <chaya.karim.omar@gmail.com>
2026-01-16 16:28:24 +0000

    AD: Filtered Search

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,5 +1,5 @@
 import string
-from .search_utils import load_movies, DEFAULT_SEARCH_LIMIT
+from .search_utils import load_movies, load_stop_words, DEFAULT_SEARCH_LIMIT
 
 def keyword_search(query):
     movies = load_movies()
@@ -20,7 +20,12 @@ def has_matching_token(query_tokens, title_tokens):
             if query_token in title_token:
                 return True
     return False
-    
+
+def remove_stop_words(tokens):
+    stop_words = load_stop_words()
+    tokens = [token for token in tokens if token not in stop_words]
+    return tokens
+
 def preprocess_text(text):
     text = text.lower()
     text = text.translate(str.maketrans('', '', string.punctuation))
@@ -30,4 +35,5 @@ def tokenize_text(text):
     text = preprocess_text(text)
     tokens = text.split()
     valid_tokens = [token for token in tokens if token != ""]
-    return valid_tokens
+    good_tokens = remove_stop_words(valid_tokens)
+    return good_tokens
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -2,6 +2,7 @@ import json, os
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 data_path = os.path.join(ROOT_DIR, "data", "movies.json")
+stop_words_path = os.path.join(ROOT_DIR, "data", "stopwords.txt")
 
 DEFAULT_SEARCH_LIMIT = 5
 
@@ -9,3 +10,9 @@ def load_movies():
     with open(data_path, 'r') as f:
         data = json.load(f)
     return data["movies"]
+
+def load_stop_words():
+    with open(stop_words_path, 'r') as f:
+        stop_words = f.read()
+        stop_words = stop_words.splitlines()
+    return stop_words
